ef2035953e5b9aecd856fb22628ea5ccec22dc15
Ryo802701 <rkawa75@gmail.com>
2022-08-26 20:56:47 +0900

    Initial commit

---
+++ src/main/java/com/example/RaiseTechJavaEx6/RaiseTechJavaEx6Application.java
@@ -0,0 +1,13 @@
+package com.example.RaiseTechJavaEx6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiseTechJavaEx6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaiseTechJavaEx6Application.class, args);
+	}
+
+}
