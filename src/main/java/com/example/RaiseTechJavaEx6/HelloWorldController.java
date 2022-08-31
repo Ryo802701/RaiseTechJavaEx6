cb9eea6dc2174cba2b42dd5a91b070b029dbc4e0
Ryo802701 <rkawa75@gmail.com>
2022-08-31 21:13:32 +0900

    レビュー反映（ソース整形、HTMLファイルを表示するコントローラを追加、@AutoWiredの箇所をコンストラクタインジェクションに変更）

--- src/main/java/com/example/RaiseTechJavaEx6/HelloWorldController.java
+++ src/main/java/com/example/RaiseTechJavaEx6/HelloWorldController.java
@@ -8,7 +8,6 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,41 +17,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/helloworld")
 public class HelloWorldController {
 
-    @Autowired
-    private MessageSource messageSource;
-	
+	private MessageSource messageSource;
+
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
 	// 文字列を出力
 	@GetMapping("/1")
 	public String hello1() {
-        return "Hello World!";
-    }
-	
+		return "Hello World!";
+	}
+
 	// HTMLを出力
 	@GetMapping("/2")
 	public void hello2(HttpServletResponse res) throws IOException {
 		LocalDateTime ldtNow = LocalDateTime.now();
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd/ HH:mm:ss.SSS");
 		String strNow = dtf.format(ldtNow);
-		
+
 		res.setContentType("text/html; charset=Shift_JIS");
-	    PrintWriter out = res.getWriter();
+		PrintWriter out = res.getWriter();
 
-	    out.println("<html>");
-	    out.println("<head>");
-	    out.println("<title>Hello World</title>");
-	    out.println("</head>");
-	    out.println("<body>");
-	    out.println("Hello World!!</br>");
-	    out.println("現在日時は " + strNow);
-	    out.println("</body>");
-	    out.println("</html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello World</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("Hello World!!</br>");
+		out.println("現在日時は " + strNow);
+		out.println("</body>");
+		out.println("</html>");
 
-	    out.close();
+		out.close();
 	}
-	
+
 	// リクエストで受け取った国（japan、us、france、korea）ごとに出すメッセージを変える
 	@GetMapping("/3")
 	public String hello3(Locale locale) {
-        return messageSource.getMessage("message", null, locale);
-    }
+		return messageSource.getMessage("message", null, locale);
+	}
 }
